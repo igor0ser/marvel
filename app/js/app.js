301ad1781166e4b0680504b405d9c8d071f72e76
Igor Sergiienko <Igor_Sergiienko@epam.com>
2016-05-23 18:47:42 +0300

    spiderman image

---
+++ app/js/app.js
@@ -0,0 +1,10 @@
+(function(){
+	'use strict';
+
+	var app = angular.module('marvel', []);
+
+	app.value('key', {
+		public: 'b46aeca62107287fa13b2564c2cabcae',
+		private: 'c4d6a1f0f098cdcc2ede8192252b426bcd921119'
+	});
+})();
